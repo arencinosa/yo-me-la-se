5f4aab0c85ebaf490df30ff91bbdc8a3a82ad74e
Alejandro Ramos Encinosa <arencinosa@gmail.com>
2020-03-28 16:05:26 -0400

    Script para generar un JSON compatible para el juego

---
+++ extra/csv2json.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'json'
+
+data = CSV.read(ARGV[0])
+db = Hash.new {|h,k| h[k] = []}
+data[1..-1].each do |row|
+    n,p,o1,o2 = row
+    db[n] << {
+        question: p,
+        options: [
+            o1,
+            o2
+        ]
+    }
+end
+
+f = open(ARGV[1], 'w')
+f.write(JSON.pretty_generate(db, {indent: '    '}))
+f.close()
